a0058ad6859bf185e8e873d21febc90f9b5d1e8a
Paulo <paulo.araujo3065@gmail.com>
2025-01-15 02:34:31 -0300

    Base do código feita

--- paulo_app/views.py
+++ paulo_app/views.py
@@ -1,6 +1,37 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Produto
+from .forms import ProdutoForm
 
 # Create your views here.
 
-def hello(request):
-    return HttpResponse('<h1>Hello, World!</h1>')
+def lista_produtos(request):
+    produtos = Produto.objects.all() .order_by('nome')
+    return render(request, 'produtos/lista_produtos.html',{'produtos':produtos})
+
+def novo_produto(request):
+    if request.method == 'POST':
+        form = ProdutoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_produtos')
+    else:
+        form = ProdutoForm()
+    return render(request, 'produtos/novo_produto.html', {'form': form})
+
+def editar_produto(request, id):
+    produto = get_object_or_404(Produto, id=id)
+    if request.method == 'POST':
+        form = ProdutoForm(request.POST, instance=produto)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_produtos')
+    else:
+        form = ProdutoForm(instance=produto)
+    return render(request, 'produtos/editar_produto.html', {'form': form})
+
+def excluir_produto(request, id):
+    produto = get_object_or_404(Produto, id=id)
+    if request.method == 'POST':
+        produto.delete()
+        return redirect('lista_produtos')
+    return render(request, 'produtos/excluir_produto.html', {'produto': produto})
--- paulo_case/urls.py
+++ paulo_case/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-from paulo_app import views
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('hello/', views.hello)
+    path('', include('paulo_app.urls')),
+    path('produtos/', include('paulo_app.urls'))
 ]
