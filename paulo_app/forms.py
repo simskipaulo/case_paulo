eed1231be0ac3712fb65945c3824bbd3eda0500e
Paulo <paulo.araujo3065@gmail.com>
2025-01-16 19:00:03 -0300

    Feat: Ajuste de erro de numero negativo

--- paulo_app/forms.py
+++ paulo_app/forms.py
@@ -5,3 +5,15 @@ class ProdutoForm(forms.ModelForm):
     class Meta:
         model = Produto
         fields = ['nome', 'preco', 'estoque']
+    
+    def clean_preco(self):
+        preco = self.cleaned_data.get('preco')
+        if preco < 0:
+            raise forms.ValidationError('Preço não pode ser negativo.')
+        return preco
+    
+    def clean_estoque(self):
+        estoque = self.cleaned_data.get('estoque')
+        if estoque < 0:
+            raise forms.ValidationError("O estoque não pode ser negativo.")
+        return estoque
--- paulo_app/models.py
+++ paulo_app/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
 class Produto(models.Model):
     nome = models.CharField(max_length=100)
-    preco = models.DecimalField(max_digits=10, decimal_places=2)
-    estoque = models.IntegerField()
+    preco = models.DecimalField(
+        max_digits=10, decimal_places=2, validators=[MinValueValidator(0,0)]
+    )
+    estoque = models.PositiveIntegerField(validators=[MinValueValidator(0)])
     criacao = models.DateField(auto_now_add=True)
     
     def __str__(self):
